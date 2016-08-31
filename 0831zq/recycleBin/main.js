e33823b25563cad6c7860963149cfa3e9926fa68
JackDemon <zq1994zb@qq.com>
2016-09-01 00:23:39 +0800

    修改BUG

--- 0831zq/recycleBin/main.js
+++ 0831zq/recycleBin/main.js
@@ -4,59 +4,69 @@
 
 $(function () {
 
+    var rr=0;
     $(".peel").draggable({
         containment:"#container",
         cursor:"pointer",
         scope:"group1",
         revert:true,
-        revertDuration:2000,
-        stop:function () {
-            alert("回收失败，请重新回收")
-        }
+        revertDuration:2000
     });
     $(".battery").draggable({
         containment:"#container",
         cursor:"pointer",
         scope:"group2",
         revert:true,
-        revertDuration:2000,
-        stop:function () {
-            alert("回收失败，请重新回收")
-        }
+        revertDuration:2000
     });
     $(".glass").draggable({
         containment:"#container",
         cursor:"pointer",
         scope:"group3",
         revert:true,
-        revertDuration:2000,
-        stop:function () {
-            alert("回收失败，请重新回收")
-        }
+        revertDuration:2000
     });
     
     $(".life").droppable({
         scope:"group1",
         drop:function () {
             alert("回收成功");
-            $(".peel").css({"display": "none"})
+            $(".peel").remove();
+            rr=1;
+        },
+        deactivate:function () {
+            if(rr==0) {
+                alert("回收失败，请重新回收")
+            }
+            rr=0;
         }
     });
     $(".recycle").droppable({
         scope:"group2",
         drop:function () {
-            // alert("回收失败,请重新回收");
             alert("回收成功");
-            $(".battery").css({"display": "none"})
+            $(".battery").remove();
+            rr=1;
+        },
+        deactivate:function () {
+            if(rr==0) {
+                alert("回收失败，请重新回收")
+            }
+            rr=0;
         }
     });
     $(".noRecycle").droppable({
         scope:"group3",
         drop:function () {
-            // alert("回收失败,请重新回收")
             alert("回收成功");
-            $(".glass").css({"display": "none"})
+            $(".glass").remove();
+            rr=1;
+        },
+        deactivate:function () {
+            if(rr==0) {
+                alert("回收失败，请重新回收")
+            }
+            rr=0;
         }
     });
-    
 });
